fdc33ed47fc7939dedc76185fbb5f476e36db73f
rickie maweu <rickiemaweu@gmail.com>
2022-07-26 14:20:24 +0300

    component props mini project

---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList(props){
+
+    const articles = props.posts.map(post => (
+      <Article
+        key = {post.id}
+        date = {post.date}
+        preview = {post.preview}
+        title = {post.title}
+      />
+
+    ));
+
+    return <main>{articles}</main>
+}
+
+export default ArticleList; 
